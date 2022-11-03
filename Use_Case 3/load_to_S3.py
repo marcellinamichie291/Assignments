0a407d45be2c4938eefae9826d4dd0f768933e90
Chaithra68 <117254575+Chaithra68@users.noreply.github.com>
2022-11-04 01:18:17 +0530

    Add files via upload

---
+++ Use_Case 3/load_to_S3.py
@@ -0,0 +1,18 @@
+import boto3
+import pandas as pd
+from configparser import ConfigParser
+	
+file='config.ini'
+config=ConfigParser()
+config.read(file)
+	
+s3=boto3.resource( service_name=config['S3']['serviceName'],
+	    			region_name=config['S3']['regionname'],
+	    			aws_access_key_id=config['S3']['aws_access_keyid'],
+	    			aws_secret_access_key=config['S3']['aws_access_key'])
+	
+# printing all the buckets name present in S3
+for bucket in s3.buckets.all():
+	print(bucket.name)
+	
+s3.Bucket(config['S3']['bucket_name']).upload_file(Filename=config['postgresql']['file_name'], Key=config['S3']['file_name'])
---
+++ Use_Case 3/postgresql_connection.py
@@ -0,0 +1,28 @@
+import psycopg2 as pg
+import pandas as pd
+from configparser import ConfigParser
+	
+file='config.ini'
+config=ConfigParser()
+config.read(file)
+
+conn = None
+try:
+	conn = pg.connect(host = config['postgresql']['hostname'],
+					  dbname = config['postgresql']['database'],
+					  user = config['postgresql']['username'],
+					  password = config['postgresql']['pwd'],
+	    			  port = config['postgresql']['port_id'])
+	cur = conn.cursor()
+	cur.execute('SELECT * FROM '+config['postgresql']['table_name'])
+	data = cur.fetchall()
+	
+	df=pd.DataFrame(data,columns=config['postgresql']['column_name'])
+	print(df)
+	df.to_csv(config['postgresql']['path']+config['postgresql']['file_name'])
+except Exception as error:
+	print(error)
+finally:
+	if conn is not None:    
+	    conn.close()
+
